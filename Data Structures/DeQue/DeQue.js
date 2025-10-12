50d4c4a67c54d6e273d93c24c4021c7a2fbc115e
Vahe Ghazaryan <vaheghazaryan122@gmail.com>
2025-10-12 14:39:44 +0400

    .

---
+++ Data Structures/DeQue/DeQue.js
@@ -0,0 +1,228 @@
+class DeQue {
+	#bucketSize = 0;
+	#blockSize = 0;
+	#map = null;
+	#size = 0;
+
+	constructor(iterable = [], initialBucketSize = 4, initialBlockSize = 8) {
+		this.#bucketSize = initialBucketSize;
+		this.#blockSize = initialBlockSize;
+
+		this.#map = Array.from({ length: this.#bucketSize }, () =>
+			Array(this.#blockSize).fill(null)
+		);
+
+		const mid = Math.floor(this.#bucketSize / 2);
+
+		this.headBlock = mid;
+		this.headIndex = 0;
+
+		this.tailBlock = mid;
+		this.tailIndex = -1;
+
+		for (let el of iterable) this.pushBack(el);
+	}
+
+	//helper
+
+	#copy_pos(block, index) {
+		return { block, index };
+	}
+
+	#write(pos, value) {
+		this.#map[pos.block][pos.index] = value;
+	}
+
+	#read(pos) {
+		return this.#map[pos.block][pos.index];
+	}
+
+	#resize() {
+		const oldMap = this.#map;
+		const oldSize = this.#bucketSize;
+
+		this.#bucketSize *= 2;
+		const newMap = Array.from({ length: this.#bucketSize }, () =>
+			Array(this.#blockSize).fill(null)
+		);
+
+		const offset = Math.floor(this.#bucketSize / 4);
+
+		for (let i = 0; i < oldSize; i++) {
+			newMap[i + offset] = oldMap[i];
+		}
+
+		this.headBlock += offset;
+		this.tailBlock += offset;
+		this.#map = newMap;
+
+		return offset;
+	}
+
+	#inc(pos) {
+		if (pos.index + 1 > this.#blockSize - 1) {
+			pos.index = 0;
+			if (pos.block + 1 > this.#bucketSize - 1) {
+				const offset = this.#resize();
+				pos.block += offset;
+			}
+			++pos.block;
+		} else {
+			++pos.index;
+		}
+	}
+
+	#dec(pos) {
+		if (pos.index - 1 < 0) {
+			pos.index = this.#blockSize - 1;
+			if (pos.block - 1 < 0) {
+				const offset = this.#resize();
+				pos.block += offset;
+			}
+			--pos.block;
+		} else {
+			--pos.index;
+		}
+	}
+
+	#swap(pos1, pos2) {
+		const tmp = this.#read(pos1);
+		this.#write(pos1, this.#read(pos2));
+		this.#write(pos2, tmp);
+	}
+
+	//interface
+
+	front() {
+		if (this.#size === 0) return null;
+		return this.#map[this.headBlock][this.headIndex];
+	}
+
+	back() {
+		if (this.#size === 0) return null;
+		return this.#map[this.tailBlock][this.tailIndex];
+	}
+
+	size() {
+		return this.#size;
+	}
+
+	pushBack(value) {
+		const pos = this.#copy_pos(this.tailBlock, this.tailIndex);
+
+		this.#inc(pos);
+		this.#write(pos, value);
+
+		this.tailBlock = pos.block;
+		this.tailIndex = pos.index;
+
+		++this.#size;
+	}
+
+	pushFront(value) {
+		const pos = this.#copy_pos(this.headBlock, this.headIndex);
+
+		this.#dec(pos);
+		this.#write(pos, value);
+
+		this.headBlock = pos.block;
+		this.headIndex = pos.index;
+
+		++this.#size;
+	}
+
+	popBack() {
+		if (this.#size === 0) return null;
+
+		const pos = this.#copy_pos(this.tailBlock, this.tailIndex);
+
+		const value = this.#read(pos);
+		this.#write(pos, null);
+
+		this.#dec(pos);
+		this.tailBlock = pos.block;
+		this.tailIndex = pos.index;
+
+		--this.#size;
+		return value;
+	}
+
+	popFront() {
+		if (this.#size === 0) return null;
+
+		const pos = this.#copy_pos(this.headBlock, this.headIndex);
+
+		const value = this.#read(pos);
+
+		this.#write(pos, null);
+
+		this.#inc(pos);
+		this.headBlock = pos.block;
+		this.headIndex = pos.index;
+
+		--this.#size;
+		return value;
+	}
+
+	at(index) {
+		if (index < 0 || index >= this.#size)
+			throw new Error("index not valid");
+
+		const pos = this.#copy_pos(this.headBlock, this.headIndex);
+
+		for (let i = 0; i < index; ++i) this.#inc(pos);
+
+		return this.#read(pos);
+	}
+
+	reverse() {
+		const leftPos = this.#copy_pos(this.headBlock, this.headIndex);
+		const rightPos = this.#copy_pos(this.tailBlock, this.tailIndex);
+
+		while (true) {
+			if (
+				leftPos.block > rightPos.block ||
+				(leftPos.block === rightPos.block &&
+					leftPos.index >= rightPos.index)
+			)
+				break;
+
+			this.#swap(leftPos, rightPos);
+
+			this.#inc(leftPos);
+			this.#dec(rightPos);
+		}
+	}
+
+	toArray() {
+		const arr = [];
+
+		for (const item of this) {
+			if (item) arr.push(item);
+		}
+
+		return arr;
+	}
+
+	visualize() {
+		for (let i = 0; i < this.#map.length; ++i) {
+			const bucket = this.#map[i];
+			if (!bucket) continue;
+
+			const display = bucket
+				.map(elem => (elem === null ? "." : elem))
+				.join(" ");
+			console.log(`Bucket_${i}: [${display}]`);
+		}
+	}
+
+	*[Symbol.iterator]() {
+		const pos = this.#copy_pos(this.headBlock, this.headIndex);
+		for (let i = 0; i < this.#size; ++i) {
+			yield this.#read(pos);
+			this.#inc(pos);
+		}
+	}
+}
+
+export { DeQue };
---
+++ Data Structures/DeQue/DeQue.md
@@ -0,0 +1,138 @@
+# DeQue
+
+`DeQue` is a custom double-ended queue (deque) implementation in JavaScript using a "bucket" block structure for efficient insertion and deletion from both ends.  
+
+---
+
+## Table of Contents
+
+- [Class Overview](#class-overview)  
+- [Constructor](#constructor)  
+- [Methods](#methods)  
+- [Examples](#examples)  
+
+---
+
+## class-overview
+
+* bucketSize – number of blocks in the deque.
+* blockSize – number of slots per block.
+* size – number of elements currently in the deque.
+* map – internal 2D array storing elements.
+* headBlock / headIndex – pointers for the front of the deque.
+* tailBlock / tailIndex – pointers for the back of the deque.
+
+## Constructor
+
+```js
+    new DeQue(iterable = [], initialBucketSize = 4, initialBlockSize = 8)
+```
+* iterable – optional array of elements to initialize the deque.
+* Initializes the internal map structure and pointers.
+
+
+## methods
+
+### pushBack(value)
+
+* Adds value to the back of the deque.
+```js
+    dq.pushBack(5);
+```
+
+### pushFront(value)
+
+* Adds value to the front of the deque.
+```js
+    dq.pushFront(0);
+```
+
+### popBack()
+
+* Removes and returns the element from the back of the deque.
+* Returns null if the deque is empty.
+```js
+    const last = dq.popBack();
+```
+
+### popFront()
+
+* Removes and returns the element from the front of the deque.
+* Returns null if the deque is empty.
+```js
+    const first = dq.popFront();
+```
+
+### at(index)
+
+* Returns the element at the given index.
+* Throws an error if the index is out of bounds.
+```js
+    const el = dq.at(2);
+```
+
+### front()
+
+* return first element;
+
+```js
+    const first = dq.front();
+```
+
+### back()
+
+* return last element;
+
+```js
+    const last = dq.back();
+```
+
+### size()
+
+* return this size.
+
+```js
+    const size = dq.size();
+```
+
+### reverse()
+
+* reverse the all elements deQue.
+
+```js
+    dq.reverse();
+```
+
+### toArray()
+
+* replace to Array
+
+```js
+    const arr = dq.toArray();
+```
+
+### visualize()
+
+* Prints all elements of the deque in order to the console.
+```js
+    dq.visualize(); // [0, 1, 2, 3] 
+```
+
+
+## Examples
+
+```js 
+import { DeQue } from './DeQue.js';
+
+const dq = new DeQue([1, 2, 3]);
+
+dq.pushFront(0);  // [0, 1, 2, 3]
+dq.pushBack(4);   // [0, 1, 2, 3, 4]
+
+dq.popFront();    // returns 0, deque: [1, 2, 3, 4]
+dq.popBack();     // returns 4, deque: [1, 2, 3]
+
+console.log(dq.at(1)); // 2
+
+dq.print(); // [1, 2, 3]
+```
